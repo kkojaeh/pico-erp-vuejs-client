b326ddbdc1958358d52e8420353d1eda2b71567b
Ethan <kkojaeh@gmail.com>
2018-06-14 09:07:00 +0900

    1.0.11

---
+++ dev-deploy.sh
@@ -0,0 +1,6 @@
+export FIREBASE_API_KEY='AIzaSyBaS7v9fhkK6Nn6MBqV02ej5T3ffdDKujc'
+export FIREBASE_PERSISTENCE='local'
+export API_BASE_URL='https://api-dev.acepk.biz/'
+export API_CONTENT_TYPE='application/vnd.acepk.v1+json'
+quasar build
+firebase deploy
